678e2233109cc3419511f5e4520684bd582adfe7
hashem <abobssam991@gmail.com>
2021-01-22 14:00:38 +0200

    final commit

---
+++ app/src/main/java/com/example/quizapp/Category.java
@@ -0,0 +1,41 @@
+package com.example.quizapp;
+
+import androidx.annotation.NonNull;
+
+public class Category {
+    public static final int PROGRAMING = 1;
+    public static final int GEOGRAPHY = 2;
+    public static final int MATH = 3;
+
+    private int id;
+    private String name;
+
+    public Category() {
+    }
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return getName();
+    }
+}
